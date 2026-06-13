a6bacf0bcee1625c7ca517543eeef0c6f14a68ad
jsjsjs0876 <1518616640@qq.com>
2026-06-13 09:04:34 +0800

    Add files via upload

---
+++ day1 review.py
@@ -0,0 +1,14 @@
+print(5>3,4<2)
+print(not True or False)
+
+
+score = 100
+if score >= 90:
+    print("niu bi")
+elif score >= 75:
+    print("hai xing")
+elif score >= 60 :
+    print("ji ge")
+else:
+    print("fei wu")        
+        

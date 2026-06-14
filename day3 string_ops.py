f3a02cce0303cf9db14999ffe37522511f781a90
jsjsjs0876 <1518616640@qq.com>
2026-06-14 16:31:46 +0800

    Add files via upload

---
+++ day3 string_ops.py
@@ -0,0 +1,18 @@
+s = "python编程"
+print(s[6])
+print(s[-8])
+#print(s[10])
+
+
+s = "人生苦短，我用python"
+print(s[0:2])
+print(s[7:13])
+print(s[3:7])
+print(s[-1::-1])
+
+
+
+s = "123456789"
+print(s[-3:-6])#get:7 because:步长默认为正
+print(s[-1::-2])
+print(s[-2::-2])

61ffe9b01757a615048d978f5f1e4d44eb02cfe5
jsjsjs0876 <1518616640@qq.com>
2026-06-15 23:01:18 +0800

    Add files via upload

---
+++ day4之晚上小练一下.py
@@ -0,0 +1,45 @@
+# o = []
+# num = [1,2,3,4,5,6,7,]
+# mix = ["n",4,6,"j",5]
+# print(num)
+# print(num[0:5:3])
+# for i in num :
+#     print(i,end ="/")
+#     print()
+# print(len(mix))  
+# mix[0] = 2
+# print(mix[0])
+# mix[0::3] = [2,99]
+# print (mix)
+# num.append([5,6,7])
+# print(num)
+# num.extend([5,5,8])
+# print(num)
+# num.insert(7,666)
+# print(num)
+# num.pop(8)
+# print(num)
+# num.pop()
+# print(num)
+# num.pop(6)
+# print(num)
+# num.remove(666)
+# print(num)
+# o = []
+# num = [[65,84,52],[68,58,64]]
+# mix = ["n",4,6,"j",5]
+# print(num)
+# re = [i for i in range(5)]
+# print(re)
+# av = (sum(s)/len(s) for s in num)
+# print(list(av))
+# num = [[65,84,52],[68,58,64]]
+# av = [sum(s)/len(s) for s in num]
+# print(av)
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# for row in matrix:
+#     print(row)
